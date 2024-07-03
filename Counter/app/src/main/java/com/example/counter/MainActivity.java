a3db82dcf096b93ef23bc2fb9fd8eb9f294d511e
yhnjiuy4321 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-07-03 12:03:24 +0800

    完成 Count app

--- Counter/app/src/main/java/com/example/counter/MainActivity.java
+++ Counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.counter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +25,29 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    public void Count(View view){
+        EditText limitEditView=(EditText)findViewById(R.id.limit);
+        TextView timesText =(TextView) findViewById(R.id.times);
+        Button countButton=(Button)findViewById(R.id.count);
+
+        int limit= Integer.parseInt(limitEditView.getText().toString());
+        int times= Integer.parseInt(timesText.getText().toString());
+
+        if(limit>times){
+            times+=1;
+            timesText.setText(String.valueOf(times));
+        }else {
+            timesText.setText("已達上限");
+            countButton.setEnabled(false);
+        }
+
+    }
+    public void Init(View view){
+        Button countButton=(Button) findViewById(R.id.count);
+        TextView timesText=(TextView) findViewById(R.id.times);
+
+        timesText.setText("0");
+        countButton.setEnabled(true);
+    }
+
 }
