1bea80bb4b11012abf3b5bf08285d7330398b754
yhnjiuy4321 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-07-08 10:53:02 +0800

    功能調整

--- Caculator/app/src/main/java/com/example/caculator/MainActivity.java
+++ Caculator/app/src/main/java/com/example/caculator/MainActivity.java
@@ -78,8 +78,11 @@ public class MainActivity extends AppCompatActivity {
             double input2 = Double.parseDouble(num2.getText().toString());
             double result;
 
-            result = input1 / input2;
-            resultText.setText(String.format(Locale.US, "%.2f", result));
+            if( input2 == 0){
+                resultText.setText("    Num2 can't be 0!");
+            }else {
+                result = input1 / input2;
+                resultText.setText(String.format(Locale.US, "%.2f", result));
+            }
         }
-
 }
