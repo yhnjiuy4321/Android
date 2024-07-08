59f6f312d27c8082e40720f1f6c023c70c61a6f8
yhnjiuy4321 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-07-08 10:37:23 +0800

    計算機功能設定

--- Caculator/app/src/main/java/com/example/caculator/MainActivity.java
+++ Caculator/app/src/main/java/com/example/caculator/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.caculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +28,58 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    
+    public void Plus(View view) {
+        EditText num1 = (EditText) findViewById(R.id.input1);
+        EditText num2 = (EditText) findViewById(R.id.input2);
+        TextView resultText = (TextView) findViewById(R.id.result);
+        //Button plusButton = (Button) findViewById(R.id.plus);
+
+        double input1 = Double.parseDouble(num1.getText().toString());
+        double input2 = Double.parseDouble(num2.getText().toString());
+        double result;
+
+        result = input1 + input2;
+        resultText.setText(String.format(Locale.US, "%.2f", result));
+    }
+
+    public void Cut(View view) {
+        EditText num1 = (EditText) findViewById(R.id.input1);
+        EditText num2 = (EditText) findViewById(R.id.input2);
+        TextView resultText = (TextView) findViewById(R.id.result);
+        //Button cutButton = (Button) findViewById(R.id.cut);
+
+        double input1 = Double.parseDouble(num1.getText().toString());
+        double input2 = Double.parseDouble(num2.getText().toString());
+        double result;
+
+        result = input1 - input2;
+        resultText.setText(String.format(Locale.US, "%.2f", result));
+    }
+
+    public void Multiply(View view) {
+        EditText num1 = (EditText) findViewById(R.id.input1);
+        EditText num2 = (EditText) findViewById(R.id.input2);
+        TextView resultText = (TextView) findViewById(R.id.result);
+
+        double input1 = Double.parseDouble(num1.getText().toString());
+        double input2 = Double.parseDouble(num2.getText().toString());
+        double result;
+
+        result = input1 * input2;
+        resultText.setText(String.format(Locale.US, "%.2f", result));
+}
+
+        public void Divide(View view) {
+            EditText num1 = (EditText) findViewById(R.id.input1);
+            EditText num2 = (EditText) findViewById(R.id.input2);
+            TextView resultText = (TextView) findViewById(R.id.result);
+
+            double input1 = Double.parseDouble(num1.getText().toString());
+            double input2 = Double.parseDouble(num2.getText().toString());
+            double result;
+
+            result = input1 / input2;
+            resultText.setText(String.format(Locale.US, "%.2f", result));
+        }
 
 }
