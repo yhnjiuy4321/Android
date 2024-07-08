445d5084228a09c8440659e039fe8f9438ac866f
yhnjiuy4321 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-07-08 11:25:34 +0800

    更新

--- Caculator/app/src/main/java/com/example/caculator/MainActivity.java
+++ Caculator/app/src/main/java/com/example/caculator/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             double input2 = Double.parseDouble(num2.getText().toString());
             double result;
 
-            if( input2 == 0){
+            if(input2 == 0){
                 resultText.setText("    Num2 can't be 0!");
             }else {
                 result = input1 / input2;
