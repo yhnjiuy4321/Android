c8d25d971abf122eff13f64b013be8520edb133d
yhnjiuy4321 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-07-29 11:11:14 +0800

    0729上課內容(ActivityCycle)

--- ActivityCycle/app/src/main/java/com/example/activitycycle/MainActivity.java
+++ ActivityCycle/app/src/main/java/com/example/activitycycle/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.activitycycle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("觀察方法", "onCreate running"); // Log.d() 是除錯用的方法，可以在 Logcat 看到
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -21,4 +26,52 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void callMAP(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=逢甲大學"));
+        startActivity(intent);
+    }
+
+    public void finish(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("觀察方法", "onStart running");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("觀察方法", "onResume running");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("觀察方法", "onPause running");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("觀察方法", "onStop running");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("觀察方法", "onDestroy running");
+        super.onDestroy();
+    }
+
+
+    @Override
+    protected void onRestart() {
+        Log.d("觀察方法", "onRestart running");
+        super.onRestart();
+    }
+
 }
